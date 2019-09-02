17167363ac3df412f882e9c1c4ec6e79169a1df3
Void <pedroaa57@gmail.com>
2019-09-02 09:58:55 -0300

    Added Main file

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import scipy
+import pandas as pd 
+from matplotlib import pyplot as plt 
+import matplotlib
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
