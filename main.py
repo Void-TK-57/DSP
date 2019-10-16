5479f2ce072be0f962d8c6493806905eedcc16de
Void <pedroaa57@gmail.com>
2019-10-16 11:23:48 -0300

    Fixed Spectrum Generation

--- main.py
+++ main.py
@@ -9,29 +9,49 @@ import matplotlib.animation as animation
 
 def main():
     # read wav file and get sampling rate and data
-    fs, data = wavfile.read('data/rock-120bpm.wav')
-    data = data[:, 0]
-    plt.plot(data)
+    fs, data = wavfile.read('data/piano_c5.wav')
+    duration = len(data)/fs
+    print(duration)
+    # number of frames
+    frames = 30*duration
+    print(frames)
+    # reshape the data per frame
+    data_per_frame = np.array_split(data, frames)
+    # return fft
+    def generate(data, fs):
+        # fft
+        fft = np.absolute( np.fft.rfft(data,  n = len(data)))
+        # get frequencies
+        freq = np.fft.fftfreq(len(fft), d = 1.0/fs)
+        # return fft and freq
+        return fft, freq
+
+    fft, freq = generate(data_per_frame[0], fs)
+    print(fft)
+    print(freq)
+    print(fft.shape, freq.shape)
+    plt.plot(freq, fft)
     plt.show()
-    # get spectrum (frequency interval, time interval and matrix of frequency per time )
-    frequency_interval, time_interval, spectrum = signal.spectrogram(data, fs = fs)
-    print(frequency_interval.shape, time_interval.shape, spectrum.shape)
+
     # create figure
     fig, ax = plt.subplots()
-
-    line_plot, = ax.plot(frequency_interval, spectrum[:, 0])
+    line_plot, = ax.plot(freq, fft)
+    ax.set_xlabel("Frequencies")
+    ax.set_ylabel("Magnitude")
 
     # function to initialize frame
     def init():
-        line_plot.set_ydata( spectrum[:, 0] )
+        data, _ = generate(data_per_frame[0], fs)
+        line_plot.set_ydata( data )
         return line_plot,
     
     def animate(i):
         # change the data for the spectrum at time i, and change title to time i
-        line_plot.set_ydata( spectrum[:, i] )
+        data, _ = generate( data_per_frame[ i % len(data_per_frame) ], fs )
+        line_plot.set_ydata( data )
         return line_plot,
     # create animation
-    ani = animation.FuncAnimation( fig, animate, init_func=init, interval=2, blit=True, save_count=50, repeat_delay = 1000)
+    ani = animation.FuncAnimation( fig, animate, init_func=init, interval=100/6, blit=True, save_count=50, repeat_delay = 1000)
     #ani.save('movie.mp4')
     plt.show()
 
