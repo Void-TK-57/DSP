8efecd5a1f47bf3ac0b4c31b1824ee6610bb191b
Void <pedroaa57@gmail.com>
2019-10-31 13:02:26 -0300

    Add initial Signal Class

--- src/R/main.r
+++ src/R/main.r
@@ -1,2 +1,11 @@
-a <- 1:20
-print(a)
+
+source("signal.r")
+
+sig <- Signal$new(c(1, 2, 3, 2, 4, 1), 1:6)
+sig$print()
+sig2 <- Signal$new(c(1, 4, 2, 3, 2, 1), 5:10)
+sig2$print()
+
+new_n <- .equalize_range(sig, sig2)
+
+print(new_n)
--- src/R/signal.r
+++ src/R/signal.r
@@ -1,3 +1,56 @@
-test <- function(a) {
-    return(a*a)
+# Main Signal Library for Digital Signal Processing
+# Author: Void-TK-57
+
+# add libraries
+library(R6)
+
+# =============================================================================================================================
+# Auxiliary Functions
+# =============================================================================================================================
+
+# method do equalize range
+.equalize_range <- function(s1, s2) {
+    # equalize n
+    n = min(c( min( s1$get_n() ), min(s2$get_n())) ): max(c( max(s1$get_n() ), max(s2$get_n())) )
+    return(n)
 }
+   
+# =============================================================================================================================
+# Main class
+# =============================================================================================================================
+
+# main class
+Signal <- R6::R6Class("Signal", public = list(
+    # constructor
+    initialize = function(x, n, ...) {
+        # check n and x is numeric and they have the same length
+        stopifnot(is.numeric(x), is.numeric(n))
+        stopifnot(length(x) ==  length(n))
+
+        # set them
+        private$n <- n
+        private$x <- x
+    },
+
+    # get n function
+    get_n = function(...) {
+        return(private$n)
+    },
+
+    # prin function
+    print = function(...) {
+        print("Signal:")
+        print("x:")
+        print(private$x)
+        print("n:")
+        print(private$n)
+    }
+
+    ),
+
+    private = list(
+        x = 1:10,
+        n = 1:10
+    )
+
+)
